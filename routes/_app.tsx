49ea40524b537ec248a4a4359e09d3f788feecc4
EthanThatOneKid <31261035+EthanThatOneKid@users.noreply.github.com>
2024-01-11 17:34:12 -0800

    remove tailwind classes

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -1,4 +1,5 @@
 import { type PageProps } from "$fresh/server.ts";
+
 export default function App({ Component }: PageProps) {
   return (
     <html>
--- routes/index.tsx
+++ routes/index.tsx
@@ -4,19 +4,19 @@ import Counter from "../islands/Counter.tsx";
 export default function Home() {
   const count = useSignal(3);
   return (
-    <div class="px-4 py-8 mx-auto bg-[#86efac]">
-      <div class="max-w-screen-md mx-auto flex flex-col items-center justify-center">
+    <div class="">
+      <div class="">
         <img
-          class="my-6"
+          class=""
           src="/logo.svg"
           width="128"
           height="128"
           alt="the Fresh logo: a sliced lemon dripping with juice"
         />
-        <h1 class="text-4xl font-bold">Welcome to Fresh</h1>
-        <p class="my-4">
+        <h1 class="">Welcome to Fresh</h1>
+        <p class="">
           Try updating this message in the
-          <code class="mx-2">./routes/index.tsx</code> file, and refresh.
+          <code class="">./routes/index.tsx</code> file, and refresh.
         </p>
         <Counter count={count} />
       </div>
