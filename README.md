1b368e39bab083efe175892f4b91b2c649b4601c
EthanThatOneKid <31261035+EthanThatOneKid@users.noreply.github.com>
2024-01-11 17:29:23 -0800

    run `deno run -A -r https://fresh.deno.dev`

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
-# deno_subhosting
-FartLabs integration with Deno Subhosting for the Deno Subhosting Hackathon https://deno.com/blog/subhosting-hackathon
+# Fresh project
+
+Your new Fresh project is ready to go. You can follow the Fresh "Getting
+Started" guide here: https://fresh.deno.dev/docs/getting-started
+
+### Usage
+
+Make sure to install Deno: https://deno.land/manual/getting_started/installation
+
+Then start the project:
+
+```
+deno task start
+```
+
+This will watch the project directory and restart as necessary.
--- main.ts
+++ main.ts
@@ -1,5 +1,13 @@
-import * as Blockly from "npm:blockly@11.0.0-beta.1/core";
+/// <reference no-default-lib="true" />
+/// <reference lib="dom" />
+/// <reference lib="dom.iterable" />
+/// <reference lib="dom.asynciterable" />
+/// <reference lib="deno.ns" />
 
-console.log("Hello World");
+import "$std/dotenv/load.ts";
 
-// TODO: https://developers.google.com/blockly/guides/create-custom-blocks/overview
+import { start } from "$fresh/server.ts";
+import manifest from "./fresh.gen.ts";
+import config from "./fresh.config.ts";
+
+await start(manifest, config);
