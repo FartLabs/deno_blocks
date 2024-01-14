c43d910702be3266d58e7a2aa3450396c9ec2129
Ethan Davidson <31261035+EthanThatOneKid@users.noreply.github.com>
2024-01-13 16:07:34 -0800

    remove unnecessary elements

--- routes/index.tsx
+++ routes/index.tsx
@@ -2,22 +2,8 @@ import TypeScriptBlocklyIsland from "#/islands/typescript_blockly_island.tsx";
 
 export default function Home() {
   return (
-    <div class="">
-      <div class="">
-        <img
-          class=""
-          src="/logo.svg"
-          width="128"
-          height="128"
-          alt="the Fresh logo: a sliced lemon dripping with juice"
-        />
-        <h1 class="">Welcome to Fresh</h1>
-        <p class="">
-          Try updating this message in the
-          <code class="">./routes/index.tsx</code> file, and refresh.
-        </p>
-        <TypeScriptBlocklyIsland />
-      </div>
-    </div>
+    <main>
+      <TypeScriptBlocklyIsland />
+    </main>
   );
 }
