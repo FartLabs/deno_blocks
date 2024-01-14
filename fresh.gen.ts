99e014630a193b44a5da589b985b48c27359bd80
Ethan Davidson <31261035+EthanThatOneKid@users.noreply.github.com>
2024-01-13 16:00:32 -0800

    remove unused endpoints

--- fresh.gen.ts
+++ fresh.gen.ts
@@ -4,8 +4,6 @@
 
 import * as $_404 from "./routes/_404.tsx";
 import * as $_app from "./routes/_app.tsx";
-import * as $api_joke from "./routes/api/joke.ts";
-import * as $greet_name_ from "./routes/greet/[name].tsx";
 import * as $index from "./routes/index.tsx";
 import * as $typescript_blockly_island from "./islands/typescript_blockly_island.tsx";
 import { type Manifest } from "$fresh/server.ts";
@@ -14,8 +12,6 @@ const manifest = {
   routes: {
     "./routes/_404.tsx": $_404,
     "./routes/_app.tsx": $_app,
-    "./routes/api/joke.ts": $api_joke,
-    "./routes/greet/[name].tsx": $greet_name_,
     "./routes/index.tsx": $index,
   },
   islands: {
