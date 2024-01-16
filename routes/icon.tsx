7f46c1c29163968b0f2bc785691f27e4546bcd9f
EthanThatOneKid <31261035+EthanThatOneKid@users.noreply.github.com>
2024-01-16 00:49:00 -0800

    fix typo in icon.tsx

--- routes/icon.tsx
+++ routes/icon.tsx
@@ -7,7 +7,7 @@ export default function DenoBlocksIconPage() {
         <DenoBlocksIcon size="min(50vw, 50vh)" />
         <br />
         <p style="font-size: 2rem; font-weight: bold; text-align: center;">
-          Credit: <a href="<https://twitter.com/_tanakaworld">@_tanakaworld</a>
+          Credit: <a href="https://twitter.com/_tanakaworld">@_tanakaworld</a>
         </p>
       </div>
     </div>
