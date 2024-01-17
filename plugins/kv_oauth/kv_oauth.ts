a7845ea43a7769157318ca20605ebeaf6bd105b3
EthanThatOneKid <31261035+EthanThatOneKid@users.noreply.github.com>
2024-01-17 04:26:54 -0800

    update kv_oauth plugin

--- plugins/kv_oauth/kv_oauth.ts
+++ plugins/kv_oauth/kv_oauth.ts
@@ -1,8 +1,23 @@
 import { createGitHubOAuthConfig, createHelpers } from "deno_kv_oauth/mod.ts";
 import type { Plugin } from "$fresh/server.ts";
 
-const { signIn, handleCallback, signOut, getSessionId } = createHelpers(
-  createGitHubOAuthConfig(),
+export const SESSION_DURATION_SECONDS = 60 * 60 * 24 * 7; // 1 week
+export const SESSION_DURATION_MS = SESSION_DURATION_SECONDS * 1e3;
+
+export const oauthConfig = createGitHubOAuthConfig();
+
+export const {
+  signIn,
+  handleCallback,
+  signOut,
+  getSessionId,
+} = createHelpers(
+  oauthConfig,
+  {
+    cookieOptions: {
+      maxAge: SESSION_DURATION_SECONDS,
+    },
+  },
 );
 
 export const kvOAuthPlugin = (_kv: Deno.Kv): Plugin => ({
@@ -10,15 +25,16 @@ export const kvOAuthPlugin = (_kv: Deno.Kv): Plugin => ({
   routes: [
     {
       path: "/signin",
-      async handler(req) {
-        return await signIn(req);
+      async handler(request) {
+        return await signIn(request);
       },
     },
     {
       path: "/callback",
-      async handler(req) {
+      async handler(request) {
         // Return object also includes `accessToken` and `sessionId` properties.
-        const { response } = await handleCallback(req);
+        const { response, sessionId, tokens } = await handleCallback(request);
+        console.log({ sessionId, tokens });
 
         // TODO: See if I need to store the access token in KV manually.
 
@@ -27,14 +43,14 @@ export const kvOAuthPlugin = (_kv: Deno.Kv): Plugin => ({
     },
     {
       path: "/signout",
-      async handler(req) {
-        return await signOut(req);
+      async handler(request) {
+        return await signOut(request);
       },
     },
     {
       path: "/protected",
-      async handler(req) {
-        return await getSessionId(req) === undefined
+      async handler(request) {
+        return await getSessionId(request) === undefined
           ? new Response("Unauthorized", { status: 401 })
           : new Response("You are allowed");
       },
