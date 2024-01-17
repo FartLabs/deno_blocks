81af58d803df3ca14100a0cf58f7150e037d7b3d
EthanThatOneKid <31261035+EthanThatOneKid@users.noreply.github.com>
2024-01-17 11:18:37 -0800

    Update _app.tsx

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -10,7 +10,7 @@ export default function App(props: PageProps) {
       <head>
         <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <title>deno_subhosting</title>
+        <title>Deno Blocks</title>
         <link rel="stylesheet" href="/styles.css" />
 
         <script src="https://unpkg.com/blockly/blockly_compressed.js"></script>
